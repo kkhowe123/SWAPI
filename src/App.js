d165a3b889c30a0100313a5eea7f279bb7352794
kkhowe123 <keanuhowe3@gmail.com>
2024-02-11 09:14:53 -0500

    created the peopledetails to pull information from the api and store them in a shoallow array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import CardDetails from './components/CardDetails';
+import PeopleDetails from './components/PeopleDetails';
 function App() {
   return (
     <div>
-      <CardDetails />
+      <PeopleDetails />
+      {/* <CardDetails /> */}
     </div>
   );
 }
