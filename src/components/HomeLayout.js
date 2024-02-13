307d39615bc5f296f3fe600897301d88e1799d41
kkhowe123 <keanuhowe3@gmail.com>
2024-02-13 08:15:48 -0500

    partially completed the homelayout

--- src/components/HomeLayout.js
+++ src/components/HomeLayout.js
@@ -1,24 +1,36 @@
+import card from "../assets/Card.svg";
+import deck from "../assets/Deck.svg";
 import React from "react";
 export default function HomeLayout (){
 
 
 return(
    
+    <>
     <div className="mt-[62px] ml-[90px]">
-    <div className="flex justify-between w-[1176px] h-[32px]" > 
+    <div className="flex justify-between w-[1176px] h-[32px] g-[16px]" > 
     
-    <div >
-        <button className="pt-[8px] pl[12px] pb-[8px] pr-[12px] bg-[#FFFFFF] h-[32px] w-[112px] rounded-sm" >All Cards</button>
-        <button>Decks</button>
+    <div className="w-[212px] h-[32px] " >
+        <button className="w-[112px] h-[32px] rounded-[4px] bg-[#FFFFFF] text-[16px]" > <img className="w-[16px] h-[16px] top-[0.5px] left[2.5px] inline" src={card} alt="card icon" /> All Cards </button>
+
+
+        <button className="ml-[8px] w-[92px] h-[32px] rounded-[4px] bg-[#E4E4E4] " >
+            <img className="inline w-[18px] h-[16px] top-[0.5px] left[2.5px] mr-[8px]" src={deck} alt="Deck Icon"/>
+            
+            Decks</button>
     </div>
     <p>SW-API Deck Builder</p>
-    <p>Keanu Howe</p>
+    <p className="border-[1px] w-[129px] h-[32px] rounded-[4px] px-[12px] py-[2px] border-[#B8B8B8]" ><h6 className="w-[105px] h-[16px] text-[16px] text-center">Keanu Howe</h6></p>
 
     </div>
-    <hr className="w-[1176px] h-[2px] border-2 border-[#B8B8B8]"/>
+    <hr className="w-[1176px] h-[2px] border-2 border-[#B8B8B8] mt-[10px]"/>
     
     </div>
-   
+    
+        <div></div>
+    
+    </>
+    
 
 );
 
